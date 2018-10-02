9ba465f9ab4f21ef95eb142e2de72b22565f1dcf
eniciodehaas <43574607+eniciodehaas@users.noreply.github.com>
2018-10-02 16:10:26 +0200

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World, Goedemiddag Hoe gaat het?!");
+        System.out.println("Hello World, Goedemiddag Hoe gaat het? met mij gaat het goed!");
     }
 }
